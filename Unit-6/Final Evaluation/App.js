54662709a1d506bbdd32956bf745c95a1971696a
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-20 21:44:00 +0530

    Add files via upload

---
+++ Unit-6/Final Evaluation/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AllRoutes from './Pages/AllRoutes';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
