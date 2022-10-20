365684b260cd608ba10830b8220ed50e1242008f
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-20 21:43:26 +0530

    Add files via upload

---
+++ Unit-6/Final Evaluation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
