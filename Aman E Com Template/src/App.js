58eec917c749fea8f314b532a6963f6856554e53
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-23 22:56:02 +0530

    Add files via upload

---
+++ Aman E Com Template/src/App.js
@@ -0,0 +1,12 @@
+import Tempapp from "./Components/Tempapp";
+import "./styles.css";
+// import { Routes, Route,Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+     <h1>Weather App</h1>
+     <Tempapp/>
+    </div>
+  );
+}
